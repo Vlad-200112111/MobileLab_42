6919b611f5581e28e25532e36a31b96e77e6e79d
Vlad-200112111 <vladislavberezin217@gmail.com>
2022-11-16 06:42:18 +0900

    first commit

---
+++ app/src/main/java/com/example/mobilelab_42/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.mobilelab_42;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView imgSplash;
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imgSplash = (ImageView) findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animation);
+        imgSplash.startAnimation(animation);
+
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
