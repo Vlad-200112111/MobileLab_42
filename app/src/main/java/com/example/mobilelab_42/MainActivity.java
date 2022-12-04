7ab794a8a2ec92cb60a304c1f4e310f21c40c41a
Vlad-200112111 <vladislavberezin217@gmail.com>
2022-12-04 18:16:58 +0900

    dfgdgsdvsdv

--- app/src/main/java/com/example/mobilelab_42/MainActivity.java
+++ app/src/main/java/com/example/mobilelab_42/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.mobilelab_42;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Объявим переменные компонентов
+    Button button;
+    TextView textView;
+
+    private DataBaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDBHelper = new DataBaseHelper(this);
+
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
+
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String product = "";
+
+                Cursor cursor = mDb.rawQuery("SELECT * FROM citizenship", null);
+                cursor.moveToFirst();
+                while (!cursor.isAfterLast()) {
+                    product += cursor.getString(1) + " | ";
+                    cursor.moveToNext();
+                }
+                cursor.close();
+
+                textView.setText(product);
+            }
+        });
     }
 }
